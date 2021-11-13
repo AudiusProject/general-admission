9fbcca6f884167c70e6e5c057b5d0400dcaeef7f
Raymond Jacobson <ray@audius.co>
2021-11-12 19:25:55 -0800

    Fix lint

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,15 +1,14 @@
 import createClient from 'ipfs-http-client'
 
-const { globSource: _globSource } = createClient
-export const globSource = _globSource
+export const globSource = createClient.globSource
 
 const IPFS_PROTOCOL = process.env.IPFS_PROTOCOL || 'http'
 const IPFS_HOST = process.env.IPFS_HOST || 'localhost'
-const IPFS_PORT = parseInt(process.env.IPFS_PORT || '5001')
+const IPFS_PORT = parseInt(process.env.IPFS_PORT || '5001', 10)
 
 console.log(`Connecting to ipfs: ${IPFS_PROTOCOL}://${IPFS_HOST}:${IPFS_PORT}`)
 
-const ipfs = createClient({ 
+const ipfs = createClient({
   protocol: IPFS_PROTOCOL,
   host: IPFS_HOST,
   port: IPFS_PORT
