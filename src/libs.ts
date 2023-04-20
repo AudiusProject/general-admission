ebf90c477bb8a2167bedf098c7d6ea060f5a0d9d
Raymond Jacobson <ray@audius.co>
2023-04-20 16:43:51 -0700

    Implement blockdiff check (#128)

--- src/libs.ts
+++ src/libs.ts
@@ -6,7 +6,9 @@ const E = process.env
  * Singleton wrapper for Audius Libs.
  * Initialized in the server start-up.
  */
+// @ts-ignore - missing types on AudiusLibs
 const libs = new AudiusLibs({
+  // @ts-ignore - missing types on AudiusLibs
   ethWeb3Config: AudiusLibs.configEthWeb3(
     E.TOKEN_ADDRESS,
     E.ETH_REGISTRY_ADDRESS,
--- src/servlets/api/index.ts
+++ src/servlets/api/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import semver from 'semver'
 import libs from '../../libs'
 import { onStartup } from '../../onStartup'
 
@@ -6,7 +7,8 @@ import { shuffle } from '../utils/helpers'
 
 const LOG_PREFIX = 'servelet: api | '
 const DISCOVERY_PROVIDER_REFRESH_INTERVAL = 60 * 1000 // one minute
-const MIN_HEALTHY_SERVICES = 10
+const MIN_HEALTHY_SERVICES = 8
+const MIN_BLOCK_DIFFERENCE = 50
 
 export const router = express.Router()
 
@@ -14,15 +16,20 @@ let usableDiscoveryProviders: string[] = []
 
 const updateDiscoveryProviders = async () => {
   const registeredVersion = await libs.ethContracts.getCurrentVersion('discovery-node')
+  console.info(LOG_PREFIX, `Registered version ${registeredVersion}`)
   let services = await libs.discoveryProvider.serviceSelector.findAll({ verbose: true })
+  console.info(LOG_PREFIX, `Found services ${JSON.stringify(services)}`)
   services = services
-    .filter((service: { version: string }) => service.version >= registeredVersion)
+    .filter((service: { version: string }) => semver.gte(service.version, registeredVersion))
+    .filter((service: { block_difference: number }) => service.block_difference <= MIN_BLOCK_DIFFERENCE)
     .map((service: { endpoint: string }) => service.endpoint)
   console.info(LOG_PREFIX, `Updating internal API hosts ${JSON.stringify(services)}`)
   // If we only have found MIN_HEALTHY_SERVICES, just show everything instead
   if (services.length > MIN_HEALTHY_SERVICES) {
+    console.info(LOG_PREFIX, `Enough services found ${services.length} > ${MIN_HEALTHY_SERVICES}`)
     usableDiscoveryProviders = services
   } else {
+    console.info(LOG_PREFIX, `Not enough healthy services found, returning everything`)
     // Get all services (no healthy check)
     const allServices =
       await libs.discoveryProvider.serviceSelector.getServices()
