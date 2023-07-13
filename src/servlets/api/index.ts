b8941826452d5ffbb4b86e2482876f18d93a91aa
Raymond Jacobson <ray@audius.co>
2023-07-13 16:23:49 -0700

    Add content node list (#131)

--- src/servlets/api/index.ts
+++ src/servlets/api/index.ts
@@ -10,9 +10,19 @@ const DISCOVERY_PROVIDER_REFRESH_INTERVAL = 60 * 1000 // one minute
 const MIN_HEALTHY_SERVICES = 8
 const MIN_BLOCK_DIFFERENCE = 50
 
+type Node = {
+  owner: string
+  endpoint: string
+  spID: number
+  type: 'content-node' | 'discovery-node',
+  blockNumber: number,
+  delegateOwnerWallet: string
+}
+
 export const router = express.Router()
 
-let usableDiscoveryProviders: string[] = []
+let usableDiscoveryProviders: Node[] = []
+let usableContentNodes: Node[] = []
 
 const updateDiscoveryProviders = async () => {
   const registeredVersion = await libs.ethContracts.getCurrentVersion('discovery-node')
@@ -22,33 +32,86 @@ const updateDiscoveryProviders = async () => {
   services = services
     .filter((service: { version: string }) => semver.gte(service.version, registeredVersion))
     .filter((service: { block_difference: number }) => service.block_difference <= MIN_BLOCK_DIFFERENCE)
-    .map((service: { endpoint: string }) => service.endpoint)
-  console.info(LOG_PREFIX, `Updating internal API hosts ${JSON.stringify(services)}`)
+  console.info(LOG_PREFIX, `Updating internal discovery node hosts ${JSON.stringify(services)}`)
   // If we only have found MIN_HEALTHY_SERVICES, just show everything instead
   if (services.length > MIN_HEALTHY_SERVICES) {
     console.info(LOG_PREFIX, `Enough services found ${services.length} > ${MIN_HEALTHY_SERVICES}`)
-    usableDiscoveryProviders = services
+    usableDiscoveryProviders = services.map((s: Node) => ({
+      owner: s.owner,
+      endpoint: s.endpoint,
+      spID: s.spID,
+      type: s.type,
+      blockNumber: s.blockNumber,
+      delegateOwnerWallet: s.delegateOwnerWallet
+    }))
   } else {
     console.info(LOG_PREFIX, `Not enough healthy services found, returning everything`)
     // Get all services (no healthy check)
     const allServices =
       await libs.discoveryProvider.serviceSelector.getServices()
-    usableDiscoveryProviders = allServices
+    usableDiscoveryProviders = allServices.map((s: Node) => ({
+      owner: s.owner,
+      endpoint: s.endpoint,
+      spID: s.spID,
+      type: s.type,
+      blockNumber: s.blockNumber,
+      delegateOwnerWallet: s.delegateOwnerWallet
+    }))
   }
+}
+
+const updateContentNodes = async () => {
+  const registeredVersion = await libs.ethContracts.getCurrentVersion('content-node')
+  console.info(LOG_PREFIX, `Registered version ${registeredVersion}`)
+  const services = await libs.ServiceProvider.listCreatorNodes()
+  usableContentNodes = services
 }
 
 onStartup(() => {
   updateDiscoveryProviders()
+  updateContentNodes()
   setInterval(() => {
     updateDiscoveryProviders()
+    updateContentNodes()
   }, DISCOVERY_PROVIDER_REFRESH_INTERVAL)
 })
 
 /**
- * Gets a randomized list of discovery service endpoints
+ * Gets a randomized list of discovery node endpoints
  */
 router.get('/', async (req: express.Request, res: express.Response) => {
-  console.info(LOG_PREFIX, `Serving API hosts: ${usableDiscoveryProviders}`)
+  const randomizedEndpoints = shuffle(usableDiscoveryProviders.map((s) => s.endpoint))
+  return res.json({ data: randomizedEndpoints })
+})
+
+/**
+ * Gets a randomized list of discovery node endpoints
+ */
+router.get('/discovery', async (req: express.Request, res: express.Response) => {
+  const randomizedEndpoints = shuffle(usableDiscoveryProviders.map((s) => s.endpoint))
+  return res.json({ data: randomizedEndpoints })
+})
+
+/**
+ * Gets a randomized list of discovery node endpoints with verbose data
+ */
+router.get('/discovery/verbose', async (req: express.Request, res: express.Response) => {
   const randomizedEndpoints = shuffle(usableDiscoveryProviders)
+  return res.json({ data: randomizedEndpoints })
+})
+
+/**
+ * Gets a randomized list of content node endpoints
+ */
+router.get('/content', async (req: express.Request, res: express.Response) => {
+  const randomizedEndpoints = shuffle(usableContentNodes.map((s) => s.endpoint))
+  return res.json({ data: randomizedEndpoints })
+})
+
+/**
+ * Gets a randomized list of content node endpoints with verbose data
+ */
+router.get('/content/verbose', async (req: express.Request, res: express.Response) => {
+  const randomizedEndpoints = shuffle(usableContentNodes)
   return res.json({ data: randomizedEndpoints })
 })
